46ae73bebd9bce096f25212d68e97a6822ca268e
mileycirusprograming <tar.inem@googlemail.com>
2018-11-09 14:39:27 +0100

    Probability

--- active-learning-interface/mysite/image/models.py
+++ active-learning-interface/mysite/image/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Image(models.Model):
-    image_path = models.CharField(max_length=200)
+    path = models.CharField(max_length=200)
 
     def __str__(self):
         return self.image_path
@@ -16,4 +16,11 @@ class Label(models.Model):
         return self.name
 
 
+class Probability(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    value = models.FloatField()
 
+    def __str__(self):
+        toString = 'for {} in image {} is {}'.format(self.label, self.image, self.value)
+        return toString
