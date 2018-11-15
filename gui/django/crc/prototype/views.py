f4e2d2e21878975f61b78e99ae4ef30a087f008d
Martin <meisoldt@googlemail.com>
2018-11-15 11:26:38 +0100

    gui prototype gets shown, no functionality
    use the url: http://localhost:8000/prototype/

--- gui/django/crc/prototype/views.py
+++ gui/django/crc/prototype/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import HttpResponse, render
 from django.template import loader
 # Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
     #return HttpResponse('main.html')
-    template = loader.get_template('templates/main.html')
-    return HttpResponse(template)
+    #template = loader.get_template('main.html')
+    #return HttpResponse(template)
+    #template = loader.get_template("proto/main.html")
+    #return HttpResponse(template.render())
+    return render(request, 'proto/main.html')
+
+   # return HttpResponse("Hello World")
