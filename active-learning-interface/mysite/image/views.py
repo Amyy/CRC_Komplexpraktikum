5759192eb4789c4f040861b4661b3b3338e67559
Martin <meisoldt@googlemail.com>
2018-11-18 17:03:01 +0100

    gui calls next_image & shows result

--- active-learning-interface/mysite/image/views.py
+++ active-learning-interface/mysite/image/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
-
+from image.models import Image
+from django.template.context import Context
+from django.template import loader
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'proto/main.html')
+    # on calling the page, get the next picture from the database
+    image = Image.objects.next_image()
+    context = {
+        'images': image
+    }
+
+    #template = loader.get_template('proto/main.html')
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'proto/main.html', context)
 
 def password(request):
     return render(request, 'proto/changePassword.html')
