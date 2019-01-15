d00e2ae850dc1b98c6278b54a2461685d422cb02
Amyy <amelie.wagner@mailbox.tu-dresden.de>
2019-01-15 20:53:08 +0100

    inserted cases for loading LABELED and loading UNLABELED data
    if labeled = True: load labels
    if labeled = False: load images only, DON'T load images that are already labeled (not sure if this is working good now)

--- Network/datasets.py
+++ Network/datasets.py
@@ -15,7 +15,7 @@ import torch.utils.data as data
 import csv
 import numpy as np
 import os
-
+from pathlib import Path
 
 
 class InstrumentDataset(data.Dataset):
@@ -25,9 +25,9 @@ class InstrumentDataset(data.Dataset):
 
     """
 
-    data_path = "/local_home/bodenstse/cholec80_1fps/frames/"
+    data_path = Path('/local_home/bodenstse/cholec80_1fps/frames')
 
-    def __init__(self,width,height,transform=None,preload=True,ops=None,opsets=None,modulo = 3):
+    def __init__(self,width,height,transform=None,preload=True,ops=None,opsets=None,modulo = 3, labeled=True):
         """Initializes a dataset
 
         :param width: width the images should be resized to
@@ -44,6 +44,7 @@ class InstrumentDataset(data.Dataset):
         self.transform = transform
         self.preload = preload
         self.modulo = modulo
+        self.labeled = labeled
 
         self. data = [] # data type: <class 'list'>, f.e. data: [{'img': None, 'labels': array([0., 0., 0., 0., 0., 0., 0.], dtype=float32), 'path': '/local_home/bodenstse/cholec80_1fps/frames/1/02/00000000.png'}]
 
@@ -92,15 +93,19 @@ class InstrumentDataset(data.Dataset):
         :param labels: label of the image to be added
 
         """
-        if self.preload:
-            self.data.append({'path': path, 'img': self._load_image(path), 'labels': labels})
-        else:
-            self.data.append({'path': path, 'img': None, 'labels': labels})
+        image_path = path
 
-        # TEST #
-        # print("data type:", type(self.data))
-        # print("data:", self.data)
-        # exit()
+        if self.labeled:
+            parts = path.parts
+            image_path = self.data_path / parts[-3] / parts[-2] / parts[-1]
+
+        print('image path', str(image_path))
+
+        if self.preload:
+            self.data.append({'path': str(path), 'img': self._load_image(str(image_path)), 'labels': labels})
+        else:
+            self.data.append({'path': str(path), 'img': None, 'labels': labels})
+
 
     def load_ops(self,ops):
         """Loads a list of OPs into the dataset
@@ -111,22 +116,68 @@ class InstrumentDataset(data.Dataset):
 
         """
 
-        for op in ops:
-            f = open(op + "/Ins.csv", "r") # f.e. op == /local_home/bodenstse/cholec80_1fps/frames/4/57 ->
-            # f == /local_home/bodenstse/cholec80_1fps/frames/4/57/Ins.csv || /mnt/g27prist/TCO/TCO-Studenten/wagnerame/CRC_Komplexpraktikum/Annotations/4/57/Ins.csv
+# ORIGINAL CODE SNIPPET:
+        # for op in ops:
+        #     f = open(op + "/Ins.csv", "r")
+        #     print(op)
+        #     reader = csv.reader(f, delimiter=',')
+        #     for i, row in enumerate(reader):
+        #         if i % self.modulo == 0:
+        #             path = op + "/%08d.png" % i
+        #             label = np.array(row[1:], dtype=np.float32)
+        #             self.add_sample(path, label)
 
-            reader = csv.reader(f, delimiter=',') # reader: reads csv file
-            for i, row in enumerate(reader):
-                if i % self.modulo == 0:
-                    # print('row:', row) # row type: <class 'list'>, f.e. row: ['0', '0', '0', '0', '0', '0', '0', '0']
+# MODIFIED CODE SNIPPET;
+        if self.labeled:
 
-                    # path = op + "/%08d.png" % i # f.e. path: /local_home/bodenstse/cholec80_1fps/frames/1/02/00000000.png (for i == 0)
-                    split = op.split('/')
-                    opset_nr = split[len(split) -2] # get opset number out of path: /mnt/g27prist/TCO/TCO-Studenten/wagnerame/CRC_Komplexpraktikum/Annotations/4/57
-                    op_nr = split[len(split) - 1]
-                    path = self.data_path + opset_nr + '/' + op_nr + "/%08d.png" % (int(row[0]) / 25)
-                    label = np.array(row[1:], dtype=np.float32) # label type: <class 'numpy.ndarray'> , f.e. label: [0. 0. 0. 0. 0. 0. 0.]
-                    self.add_sample(path,label)
+            for op in ops:
+                f = open(op + "/Ins.csv", "r") # f.e. op == /local_home/bodenstse/cholec80_1fps/frames/4/57 ->
+                # f == /local_home/bodenstse/cholec80_1fps/frames/4/57/Ins.csv || /mnt/g27prist/TCO/TCO-Studenten/wagnerame/CRC_Komplexpraktikum/Annotations/4/57/Ins.csv
+
+                reader = csv.reader(f, delimiter=',') # reader: reads csv file
+                for i, row in enumerate(reader):
+                    if i % self.modulo == 0:
+                        # print('row:', row) # row type: <class 'list'>, f.e. row: ['0', '0', '0', '0', '0', '0', '0', '0']
+
+                        # path = op + "/%08d.png" % i # f.e. path: /local_home/bodenstse/cholec80_1fps/frames/1/02/00000000.png (for i == 0)
+                        split = op.split('/')
+                        opset_nr = split[len(split) -2] # get opset number out of path: /mnt/g27prist/TCO/TCO-Studenten/wagnerame/CRC_Komplexpraktikum/Annotations/4/57
+                        op_nr = split[len(split) - 1]
+                        path = self.data_path / opset_nr / op_nr / ("%08d.png" % (int(row[0]) / 25))
+
+                        label = np.array(row[1:], dtype=np.float32) # label type: <class 'numpy.ndarray'> , f.e. label: [0. 0. 0. 0. 0. 0. 0.]
+
+                        self.add_sample(path,label)
+
+        else:
+
+
+            for op in ops:
+
+                # load all images out of folder EXCEPT .csv file
+                images_paths = [x for x in Path(op).glob("*") if x not in Path(op).glob("*.csv")]
+
+                f = open(op + "/Ins.csv", "r")
+
+                reader = csv.reader(f, delimiter=',') # reader: reads csv file
+                labeled_image_path = []
+                for i, row in enumerate(reader):
+                    if i % self.modulo == 0:
+                        split = op.split('/')
+                        opset_nr = split[len(split) -2] # get opset number out of path: /mnt/g27prist/TCO/TCO-Studenten/wagnerame/CRC_Komplexpraktikum/Annotations/4/57
+                        op_nr = split[len(split) - 1]
+                        labeled_image_path.append(self.data_path / opset_nr / op_nr / ("%08d.png" % (int(row[0]) / 25)))
+
+                images_paths = [x for x in images_paths if x not in labeled_image_path]
+
+
+
+                for image_path in images_paths:
+
+                        label = np.zeros(7, dtype=np.float32)
+                        self.add_sample(image_path, label)
+
+                        print(image_path)
 
 
     def load_opsets(self,opsets):
