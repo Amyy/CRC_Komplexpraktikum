446cbab60a953d203c898701c313fc93f4b8f7e4
mileycirusprograming <tar.inem@googlemail.com>
2019-01-25 11:46:37 +0100

    changed path contious

--- active-learning-interface/script/continous.py
+++ active-learning-interface/script/continous.py
@@ -7,14 +7,15 @@ ops = ["2"]
 
 op_data = [(1, 1), (1, 2), (2, 1), (2, 2)]
 
-while True:
+if True:
+    print ('iteration start')
 
     # get new labels
     for op in op_data:
         request.get(op[0], op[1])
 
     # run training
-    exec(open("./pseudo_active_labeling.py").read())
+    #exec(open("./pseudo_active_labeling.py").read())
 
     # upload results
-    request.post("variances.csv")
+    request.post("../../Network/variances/variances.csv")
